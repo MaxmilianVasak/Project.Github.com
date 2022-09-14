5cd60c9500143a49c0bace293240d7d54dd271eb
MaxmilianVasak <Maxmilian.Vasak@educanet.cz>
2022-09-14 10:37:06 +0200

    Prvni velky commit

---
+++ Druhalekce.py
@@ -0,0 +1,64 @@
+a = 1
+a = a + 1
+print(a)
+b = 3
+c = b / a
+print(c)
+a = 1
+A = 2
+print(A , a)
+#mezera
+print("Hello world!")
+#mezera
+print("Já jsem první řádka")
+print("Já jsem druhá")
+print("Já jsem první část textu", "a já druhá")
+print("Já jsem první část textu" + " " + "a já jsem druhá!" )
+#mezera
+print("Ahoj, jsem virtuální papoušek Zdeněk, rád opakuji!")
+slovo = input("Napiš něco :") #vstup pro uživatele
+výsledek = slovo + "" + slovo
+print(výsledek)
+#mezera
+print("Zdvojnásobovač!")
+číslo = int(input("Zadej číslo :"))
+print("Jeho dvojnásobek je:", 2 * číslo)
+#mezera
+print("Vítej v kalkulačce /n")
+prvni_cislo = float (input("Zadej první číslo :"))
+druhe_cislo = float (input("Zadej druhé číslo :"))
+print("Součet:", prvni_cislo + druhe_cislo)
+print("Rozdíl:", prvni_cislo - druhe_cislo)
+print("Součin:", prvni_cislo * druhe_cislo)
+print("Podíl:", prvni_cislo / druhe_cislo)
+print("/n Děkuji za použití kalkulačky")
+#mezera
+jmeno = (input("Zadej svoje jméno :"))
+jaky = input("Zadej jaky jsi :")
+print(jmeno + " je " + jaky)
+#mezera
+cislo = float(input("Číslo k umocnění :"))
+vysledek = cislo * cislo
+print ("výsledek :", vysledek)
+#mezera
+polomer = float(input("Zadej poloměr kruhu (cm) :"))
+obvod = 2 * 3.14 * polomer
+obsah = 3.14 * (polomer * polomer)
+print (f"Obvod je : {obvod}")
+print (f"Obsah je : {obsah}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
